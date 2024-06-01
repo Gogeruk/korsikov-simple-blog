9779269ab899a9a99c1a4fad0a3f6c2e4076b700
Mykyta <korsikov_php@upiple.com>
2024-06-01 09:46:19 +0300

    [BLOG] added blog functionality.

---
+++ app/src/Controller/BlogController.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Service\MarkdownParserService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Finder\Finder;
+use Parsedown;
+
+/**
+ * Class BlogController
+ * @package App\Controller
+ */
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'blog_page', methods: ['GET'])]
+    public function blogPage(Request $request, MarkdownParserService $markdownParser): Response
+    {
+        // get tags from query string
+        $tag = $request->query->get('tag');
+
+        // get all markdown files
+        $markdownDirectory = $this->getParameter('kernel.project_dir') . '/markdown';
+        $finder = new Finder();
+        $finder->files()->in($markdownDirectory)->name('*.md');
+
+        // parse the markdown files
+        $posts = [];
+        $allTags = [];
+        foreach ($finder as /** @var SplFileInfo $file */ $file) {
+            $content = file_get_contents($file->getRealPath());
+            $metadata = $markdownParser->parse($content)[0] ?? [];
+
+            if (isset($metadata['tags'])) {
+                $allTags = array_merge($allTags, $metadata['tags']);
+            }
+
+            // when tag is set and the post does not have the tag
+            if ($tag && (!isset($metadata['tags']) || !in_array($tag, $metadata['tags']))) {
+                continue;
+            }
+
+            $slug = $file->getBasename('.md');
+            $posts[] = [
+                'slug'  => $slug,
+                'title' => $metadata['title'] ?? ucfirst($slug),
+                'tags'  => $metadata['tags'] ?? [],
+            ];
+        }
+
+        // remove duplicate tags
+        $allTags = array_unique($allTags);
+
+        return $this->render('blog/index.html.twig', [
+            'posts'       => $posts,
+            'all_tags'    => $allTags,
+            'current_tag' => $tag,
+            'title'       => null,
+            'content'     => null,
+            'tags'        => [],
+        ]);
+    }
+
+    #[Route('/blog/{slug}', name: 'blog_post', methods: ['GET'])]
+    public function blogPost(string $slug, MarkdownParserService $markdownParser): Response
+    {
+        // get the markdown file
+        $markdownDirectory = $this->getParameter('kernel.project_dir') . '/markdown';
+        $filePath = $markdownDirectory . '/' . $slug . '.md';
+
+        // when the file does not exist
+        if (!file_exists($filePath)) {
+            throw $this->createNotFoundException('The blog post does not exist');
+        }
+
+        // parse the markdown file
+        $content = file_get_contents($filePath);
+        [$metadata, $markdown] = $markdownParser->parse($content);
+        $parsedown = new Parsedown();
+        $htmlContent = $parsedown->text($markdown);
+
+        return $this->render('blog/index.html.twig', [
+            'content'     => $htmlContent,
+            'title'       => $metadata['title'] ?? ucfirst($slug),
+            'tags'        => $metadata['tags'] ?? [],
+            'posts'       => null,
+            'all_tags'    => null,
+            'current_tag' => null
+        ]);
+    }
+
+    #[Route('/api/v1/blog', name: 'api_v1_blog_list', methods: ['GET'])]
+    public function apiBlogList(Request $request, MarkdownParserService $markdownParser): JsonResponse
+    {
+        // get tags from query string
+        $tag = $request->query->get('tag');
+
+        // get all markdown files
+        $markdownDirectory = $this->getParameter('kernel.project_dir') . '/markdown';
+        $finder = new Finder();
+        $finder->files()->in($markdownDirectory)->name('*.md');
+
+        // parse the markdown files
+        $posts = [];
+        foreach ($finder as /** @var SplFileInfo $file */ $file) {
+            $content = file_get_contents($file->getRealPath());
+            $metadata = $markdownParser->parse($content)[0] ?? [];
+
+            // when tag is set and the post does not have the tag
+            if ($tag && (!isset($metadata['tags']) || !in_array($tag, $metadata['tags']))) {
+                continue;
+            }
+
+            $slug = $file->getBasename('.md');
+            $posts[] = [
+                'slug'  => $slug,
+                'title' => $metadata['title'] ?? ucfirst($slug),
+                'tags'  => $metadata['tags'] ?? [],
+            ];
+        }
+
+        return new JsonResponse($posts);
+    }
+}
---
+++ app/src/Service/MarkdownParserService.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class MarkdownParserService
+ * @package App\Service
+ */
+class MarkdownParserService
+{
+    /**
+     * Parse the markdown content
+     *
+     * @param string $content
+     * @return array
+     */
+    public function parse(string $content): array
+    {
+        if (preg_match('/^---(.*?)---\s*(.*)$/s', $content, $matches)) {
+            $metadata = Yaml::parse($matches[1]);
+            $markdown = $matches[2];
+        } else {
+            $metadata = [];
+            $markdown = $content;
+        }
+
+        return [$metadata, $markdown];
+    }
+}
